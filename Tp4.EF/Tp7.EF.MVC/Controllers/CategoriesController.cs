63638cb0020847a638411ed7e5264f8382e3dfc4
Macarena Mellone <macarenamellone@gmail.com>
2022-06-21 14:09:47 -0300

    Se corrige código

---
+++ Tp4.EF/Tp7.EF.MVC/Controllers/CategoriesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tp4.EF.Logic;
+
+namespace Tp7.EF.MVC.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        // GET: Categories
+        public ActionResult Index()
+        {
+            var logic = new CategoriesLogic();
+            List<Tp4.EF.Entities.Categories> categories = logic.GetAll();
+            return View(categories);
+        }
+    }
+}
