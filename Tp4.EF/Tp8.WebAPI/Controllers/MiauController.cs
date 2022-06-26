fa046038dde834795aa010501caa2c9756c2c9ee
Macarena Mellone <macarenamellone@gmail.com>
2022-06-26 19:34:51 -0300

    Se actualizó codigó

---
+++ Tp4.EF/Tp8.WebAPI/Controllers/MiauController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tp8.WebAPI.Controllers
+{
+    public class MiauController : Controller
+    {
+        // GET: Miau
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Cat()
+        {
+            return Redirect("https://cataas.com/cat");
+        }
+        public ActionResult CatSaysHi()
+        {
+            return Redirect("https://cataas.com/cat/says/Holi?size=:size&");
+        }
+        public ActionResult CatGif()
+        {
+            return Redirect("https://cataas.com/cat/gif");
+        }
+        public ActionResult CatGifSaysHi()
+        {
+            return Redirect("https://cataas.com/c/gif/s/Holis?fi=sepia&c=orange&s=40&t=or");
+        }
+
+    }
+}
